240bc08a8c622178fcbd7de5ef26ceaddd2ca26d
AnuAleMagar <anualemagar212@gmail.com>
2026-02-04 10:11:36 +0545

    file change

--- frontend/src/components/NoteList.jsx
+++ frontend/src/components/NoteList.jsx
@@ -5,7 +5,7 @@ export default function NoteList({
   searchQuery,
   testId,
 }) {
-  // Filter notes based on search query dfdf
+  // Filter notes based on search query
   const filteredNotes = notes.filter(
     (note) =>
       note.title.toLowerCase().includes(searchQuery.toLowerCase()) ||
