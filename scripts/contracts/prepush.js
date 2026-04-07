73c71b452f762a0f3ae8c59500c09b10750296a1
AnuAleMagar <anualemagar212@gmail.com>
2026-04-07 19:29:16 +0545

    husky rules added

--- scripts/contracts/prepush.js
+++
@@ -1,9 +0,0 @@
-import { validateStaticContracts } from "./common.js";
-
-try {
-  validateStaticContracts();
-  console.log("Pre-push contracts passed.");
-} catch (error) {
-  console.error(`\nContract check failed: ${error.message}`);
-  process.exit(1);
-}
